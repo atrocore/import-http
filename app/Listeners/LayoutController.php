ebe92345c59545a0e85bd74df80dbe1fba52e780
r.ratsun <r.ratsun@gmail.com>
2022-10-21 09:29:46 +0300

    .

--- app/Listeners/LayoutController.php
+++ app/Listeners/LayoutController.php
@@ -46,7 +46,7 @@ class LayoutController extends AbstractListener
         $result = Json::decode($event->getArgument('result'), true);
 
         $result[1]['rows'][] = [['name' => 'adapter'], false];
-        $result[1]['rows'][] = [['name' => 'httpUrl'], false];
+        $result[1]['rows'][] = [['name' => 'httpMethod'], ['name' => 'httpUrl']];
 
         $event->setArgument('result', Json::encode($result));
     }
