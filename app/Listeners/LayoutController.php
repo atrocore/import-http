4259d469ad6a9ade8e4d082fcb4575616aace731
r.ratsun <r.ratsun@gmail.com>
2023-01-26 12:20:39 +0200

    .

--- app/Listeners/LayoutController.php
+++ app/Listeners/LayoutController.php
@@ -47,7 +47,7 @@ class LayoutController extends AbstractListener
 
         $result[1]['rows'][] = [['name' => 'httpOffset'], ['name' => 'httpLimit']];
         $result[1]['rows'][] = [['name' => 'httpTotal'], false];
-        $result[1]['rows'][] = [['name' => 'httpMethod'], false];
+        $result[1]['rows'][] = [['name' => 'httpMethod'], ['name' => 'httpConnectionId']];
         $result[1]['rows'][] = [['name' => 'httpUrl', 'fullWidth' => true]];
         $result[1]['rows'][] = [['name' => 'httpBody', 'fullWidth' => true]];
 
