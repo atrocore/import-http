331bbc9cb00668c3122d141f593d7338b3fc7d00
dzoukou <dzoukouraoul@gmail.com>
2025-01-20 11:32:14 +0100

    .

--- app/Listeners/Layout.php
+++ app/Listeners/Layout.php
@@ -13,28 +13,16 @@ declare(strict_types=1);
 
 namespace ImportHttp\Listeners;
 
+use Atro\Listeners\AbstractLayoutListener;
 use Espo\Core\Utils\Json;
 use Espo\Core\EventManager\Event;
 use Espo\Listeners\AbstractListener;
 
-class LayoutController extends AbstractListener
+class Layout extends AbstractLayoutListener
 {
-    public function afterActionRead(Event $event): void
-    {
-        $scope = $event->getArgument('params')['scope'];
-
-        $name = $event->getArgument('params')['name'];
-
-        $method = 'modify' . $scope . ucfirst($name);
-
-        if (method_exists($this, $method)) {
-            $this->{$method}($event);
-        }
-    }
-
     protected function modifyImportFeedDetail(Event $event): void
     {
-        $result = Json::decode($event->getArgument('result'), true);
+        $result = $event->getArgument('result');
 
         $result[1]['rows'][] = [['name' => 'httpOffset'], ['name' => 'httpLimit']];
         $result[1]['rows'][] = [['name' => 'httpTotal'], false];
@@ -43,15 +31,15 @@ class LayoutController extends AbstractListener
         $result[1]['rows'][] = [['name' => 'httpBody', 'fullWidth' => true]];
         $result[1]['rows'][] = [['name' => 'mergeResponses'], false];
 
-        $event->setArgument('result', Json::encode($result));
+        $event->setArgument('result',  $result);
     }
 
     protected function modifyImportFeedRelationships(Event $event): void
     {
-        $result = Json::decode($event->getArgument('result'), true);
+        $result = $event->getArgument('result');
 
         $result = array_merge([['name' => 'importHttpHeaders', 'canClose' => false]], $result);
 
-        $event->setArgument('result', Json::encode($result));
+        $event->setArgument('result',  $result);
     }
 }
